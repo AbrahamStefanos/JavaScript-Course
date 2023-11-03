0e3ed38240616281b81290cda24b90765bd80f27
AbrahamStefanos <abrahamstefanos2003@gmail.com>
2023-11-03 15:46:48 -0400

    Chapter 13: Functions

--- js/main.js
+++ js/main.js
@@ -1,14 +1,5 @@
-let name = "Dave";
-let counter = 0;
-let myLetter;
-while (counter <= 3) {
-  myLetter = name[counter];
-  console.log(myLetter);
-  if (counter === 1) {
-    counter += 2;
-    continue;
-  }
-  if (myLetter === "v") break;
-  counter++;
-}
-console.log(counter);
+const toProperCase = (name) => {
+  return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+};
+
+console.log(toProperCase("september"));
