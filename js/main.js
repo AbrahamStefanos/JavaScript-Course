a976c8cf81636f9aad40ef5977c221b6ae68a452
AbrahamStefanos <abrahamstefanos2003@gmail.com>
2023-11-03 00:53:28 -0400

    Chapter 9: Ternary Operators

--- js/main.js
+++ js/main.js
@@ -1,28 +1,13 @@
-let playerOne = "scissors";
-let computer = "rock";
-
-switch (playerOne) {
-  case computer:
-    console.log("Tie game!");
-    break;
-  case "rock":
-    if (computer === "paper") {
-      console.log("computer wins!");
-    } else {
-      console.log("playerOne wins!");
-    }
-    break;
-  case "paper":
-    if (computer === "scissors") {
-      console.log("computer wins!");
-    } else {
-      console.log("playerOne wins!");
-    }
-    break;
-  default:
-    if (computer === "rock") {
-      console.log("computer wins!");
-    } else {
-      console.log("playerOne wins!");
-    }
-}
+let playerOne = "rock";
+let computer = "scissors";
+let result =
+  playerOne === computer
+    ? "Tie game!"
+    : playerOne === "rock" && computer === "paper"
+    ? "Computer wins!"
+    : playerOne === "paper" && computer === "scissors"
+    ? "Computer wins!"
+    : playerOne === "scissors" && computer === "rock"
+    ? "Computer wins!"
+    : "playerOne wins!";
+  console.log(result);  
