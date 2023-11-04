a329e33ccac2f9a0c98e8ab0fc2c2f970097dd81
AbrahamStefanos <abrahamstefanos2003@gmail.com>
2023-11-03 22:29:59 -0400

    Chapter 14: Scope

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
-const toProperCase = (name) => {
-  return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
-};
+var x = 1;
+let y = 2;
+const z = 3;
 
-console.log(toProperCase("september"));
+console.log(`global: ${x}`);
+console.log(`global: ${y}`);
+console.log(`global: ${z}`);
+
+function myFunc() {
+  var x = 10;
+  const z = 5;
+  {
+    var x = 11;
+    const z = 6;
+    console.log(`block: ${x}`);
+    console.log(`block: ${y}`);
+    console.log(`block: ${z}`);
+  }
+  console.log(`function: ${x}`);
+  console.log(`function: ${y}`);
+  console.log(`function: ${z}`);
+
+  
+}
+
+myFunc();
