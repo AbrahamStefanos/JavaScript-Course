e75a9e7b7ce599de850aaab78bc4998d7ec9fd51
AbrahamStefanos <abrahamstefanos2003@gmail.com>
2023-11-03 23:40:05 -0400

    Chapter 16: Refactored Game With Functions

--- js/main.js
+++ js/main.js
@@ -1,18 +1,101 @@
-const equipShelfA = ["baseball", "football", "volleyball"];
-const equipShelfB = ["basketball", "golf balls", "tennis balls"];
+const initGame = () => {
+  const startGame = confirm("Shall we play rock, paper, or scissors?");
+  startGame ? playGame() : alert("Ok, maybe next time.");
+};
 
-const clothesShelfA = ["tank tops", "t-shirts", "jerseys"];
-const clothesShelfB = ["sweat tops", "sweat pants", "hoodies"];
+// Game flow function
+const playGame = () => {
+  while (true) {
+    let playerChoice = getPlayerChoice();
+    playerChoice = formatPlayerChoice(playerChoice);
+    if (playerChoice === "") {
+      invalidChoice();
+      continue;
+    }
+    if (!playerChoice) {
+      decidedNotToPlay();
+      break;
+    }
+    playerChoice = evaluatePlayerChoice(playerChoice);
+    if (!playerChoice) {
+      invalidChoice();
+      continue;
+    }
+    const computerChoice = getComputerChoice();
+    const result = determineWinner(playerChoice, computerChoice);
+    displayResult(result);
+    if (askToPlayAgain()) {
+      continue;
+    } else {
+      thanksForPlaying();
+      break;
+    }
+  }
+};
 
-console.log(equipShelfA[1]);
-console.log(clothesShelfB[0]);
+const getPlayerChoice = () => {
+  return prompt("Please enter rock, paper, or scissors.");
+};
 
-const equipDept = [equipShelfA, equipShelfB];
-const clothesDept = [clothesShelfA, clothesShelfB];
+const formatPlayerChoice = (playerChoice) => {
+  if (playerChoice || playerChoice === "") {
+    return playerChoice.trim().toLowerCase();
+  } else {
+    return false;
+  }
+};
 
-console.log(equipDept[0][1]);
-console.log(clothesDept[1][0]);
+const decidedNotToPlay = () => {
+  alert("I guess you changed your mind. Maybe next time.");
+};
 
-const sportsStore = [equipDept, clothesDept];
-console.log(sportsStore[0][0][1]);
-console.log(sportsStore[1][1][0]);
+const evaluatePlayerChoice = (playerChoice) => {
+  if (
+    playerChoice === "rock" ||
+    playerChoice === "paper" ||
+    playerChoice === "scissors"
+  ) {
+    return playerChoice;
+  } else {
+    return false;
+  }
+};
+
+const invalidChoice = () => {
+  alert("You didn't enter rock, paper, or scissors.");
+};
+
+const getComputerChoice = () => {
+  const randomNumber = Math.floor(Math.random() * 3);
+  const rpsArray = ["rock", "paper", "scissors"];
+  return rpsArray[randomNumber];
+};
+
+const determineWinner = (player, computer) => {
+  const winner =
+    player === computer
+      ? "Tie game!"
+      : player === "rock" && computer === "paper"
+      ? `playerOne: ${player}\nComputer: ${computer}\nComputer wins!`
+      : player === "paper" && computer === "scissors"
+      ? `playerOne: ${player}\nComputer: ${computer}\nComputer wins!`
+      : player === "scissors" && computer === "rock"
+      ? `playerOne: ${player}\nComputer: ${computer}\nComputer wins!`
+      : `playerOne: ${player}\nComputer: ${computer}\nplayerOne wins!`;
+
+  return winner;
+};
+
+const displayResult = (result) => {
+  alert(result);
+};
+
+const askToPlayAgain = () => {
+  return confirm("Play Again?");
+};
+
+const thanksForPlaying = () => {
+  alert("Ok, thanks for playing.");
+};
+
+initGame();
